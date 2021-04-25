d38baa74a2d71e69e70f020a30146e187ca7a06c
Derik-son <taras.timoshuk93@gmail.com>
2021-04-25 14:22:15 +0300

    fix-demen-array

--- unit12/unit_12.js
+++ unit12/unit_12.js
@@ -286,7 +286,7 @@ let a15 = [
 ];
 
 function f15() {
-    let max = a15[0];
+    let max = a15[0].length;
     for(let i=0; i < a15.length; i++){
         if(a15[i].length > max){
             max = a15[i].length;
